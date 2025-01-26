66f5aa553bf1fba51b0ec38f35186238280eca42
d1mKaKiselev <polizugh241@gmail.com>
2025-01-26 14:38:42 +0300

    added scene

---
+++ src/Assets/Scripts/Player2D.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+public class Player2D : MonoBehaviour
+
+{
+    private Rigidbody2D _physics;
+    private bool _isGround;
+    private float jump = 5;
+    private SpriteRenderer flip;
+
+
+    // Start is called before the                                                                                                                                                                              
+    void Start()
+    {
+        _physics = GetComponent<Rigidbody2D>();
+        flip = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var horizontal = Input.GetAxis("Horizontal");
+
+        if (horizontal > 0)
+        {
+            flip.flipX = false;
+        }
+        if (horizontal < 0)
+            flip.flipX = true;
+
+            if (Input.GetKeyDown(KeyCode.Space) && _isGround)
+        {
+            _physics.AddForce(Vector2.up*jump, ForceMode2D.Impulse);
+
+
+        }
+        var vector = new Vector2(x: horizontal, y: _physics.velocity.y);
+        _physics.velocity = vector;
+    }
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        _isGround = true;
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    { _isGround = false; }
+
+
+
+
+}
+
