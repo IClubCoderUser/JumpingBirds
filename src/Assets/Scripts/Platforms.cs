8f5fe8fff7f79d3f8bad312b370ea37c1e8a13bc
d1mKaKiselev <polizugh241@gmail.com>
2025-03-23 15:37:40 +0300

    дВИГАЮЩИЕСЯ Платфоормы

---
+++ src/Assets/Scripts/Platforms.cs
@@ -0,0 +1,48 @@
+using UnityEditor;
+using UnityEngine;
+
+public class Platforms : MonoBehaviour
+
+{
+    public Vector3[] Target;
+    private int _pointer;
+
+    public float _spped;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var targetPosition = Target[_pointer];
+        var distance = Vector2.Distance(transform.position, targetPosition);
+        transform.position = Vector3.Lerp(transform.position, targetPosition, 1 / distance * _spped * Time.fixedDeltaTime);
+
+        if (distance <= 0.1f)
+        {
+            _pointer++;
+            if (_pointer >= Target.Length)
+            {
+                _pointer = 0;
+            }
+        }
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        var oldPositin = transform.position;
+        foreach (var item in Target)
+        {
+            Gizmos.DrawLine(oldPositin, item);
+            var pos = Vector2.Lerp(oldPositin, item, 0.5f);
+            var dist = Vector2.Distance(oldPositin, item);
+            oldPositin = item;
+            Gizmos.DrawCube(item, Vector2.one / 5);
+            Handles.Label(pos, dist.ToString());
+        }
+    }
+
+}
